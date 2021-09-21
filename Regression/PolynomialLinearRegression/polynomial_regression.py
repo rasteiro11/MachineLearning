e48a5faea85a2c37a28b1ae9afa964deedcdf686
Rafael Melloni Chacon Arnone <titico0306@gmail.com>
2021-09-21 18:47:58 -0300

    initial commit

--- Regression/PolynomialLinearRegression/polynomial_regression.py
+++ Regression/PolynomialLinearRegression/polynomial_regression.py
@@ -1,21 +1,14 @@
 # Polynomial Regression
 
-# conda activate tf_gpu
-# python
-
-
 # Importing the libraries
-import numpy as np 
-import matplotlib.pyplot as plt 
-import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
-
-# print(X)
-
 
 # Training the Linear Regression model on the whole dataset
 from sklearn.linear_model import LinearRegression
